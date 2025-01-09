6b5a4188734152c7cf1040053c711f999ad6050e
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-01-09 13:52:13 +0530

    Home page

---
+++ to-do/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+// import Sigup from "../Route/Sigup"
+
+const Home=()=>{
+    return(
+        <section className="container">
+     
+            {/* <Sigup /> */}
+        </section>
+    )
+}
+
+export default Home;
