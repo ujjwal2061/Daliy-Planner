9cc05de09cb3215ba5949060defb6d66cce16f89
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-01-15 02:09:27 +0530

    All good

--- to-do/src/App.jsx
+++ to-do/src/App.jsx
@@ -1,11 +1,13 @@
-// import Home from './pages/Home';
+
  import AppRoute from './Route/AppRoute';
 import { FirebaseProvider } from './firebase/Firebase';
+import ThemeProvider from './theme/Theme';
  const App=()=>{
   return(
     <FirebaseProvider>
+      <ThemeProvider>
       <AppRoute />
-    {/* <Home /> */}
+      </ThemeProvider>
     </FirebaseProvider>
   )}
   export default App;
--- to-do/src/pages/Home.jsx
+++ to-do/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-// import Sigup from "../Route/Sigup"
-// import Login from "../Route/Login"
 import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import {Menu} from 'lucide-react'
 import { CircleChevronRight } from 'lucide-react';
+import { ThemeContext } from "../theme/Theme";
+import { useContext } from "react";
 const Home=()=>{
+  const {theme ,setTheme}=useContext(ThemeContext);
   const [isshow ,setIsshow]=useState(false)
     const links=[
       {
@@ -21,31 +22,39 @@ const Home=()=>{
     const toogle=()=>{
       setIsshow(prevState=>!prevState)
     }
-
-    // Function add the To-Do
-
+   const ThemeToggle=()=>{
+    setTheme((prevtheme)=>(prevtheme==="light"?"dark":"light"))
+   }
     return(
-        <section className="min-h-screen bg-background">
-        <nav className="  ml-6 md:ml-11 mt-2  w-[90%] flex justify-between  items-center px-5 bg-zinc-800   py-2  border-2  rounded-lg fixed left-0 right-0 z-50   transition-all duration-300 ease-in-out shadow-md">
+      <section className={`min-h-screen ${theme === "dark" ? "bg-zinc-900" : "bg-background"}`}>
+        <nav className={`  ${theme==='dark' ? "bg-background":"bg-white"}  ml-6 md:ml-11 mt-2  w-[90%] flex justify-between  items-center px-5 bg-zinc-800   py-2  border-2  rounded-lg fixed left-0 right-0 z-50   transition-all duration-300 ease-in-out shadow-md`}>
           <NavLink to="/" className="text-xl font-atma font-semibold text-white "> Daliy Planner</NavLink>
           <div className=" items-center text-white gap-20  hidden md:flex  w-32 ">
             {links.map((items)=>(
               <NavLink key={items.id} to={items.link} className=" font-semibold text-[17px] rounded-md px-3 hover:px-3 " >{items.name}</NavLink>
             ))}
           </div>
-          <NavLink to="/content" ><p className="rounded-md px-2 py-1 b border-2 bg-zinc-100 text-black font-medium  cursor-pointer">Start Now</p></NavLink>
+          {/* Theme Section */}
+          <div className="flex  items-center md:gap-6 gap-2">
+           <spna onClick={ThemeToggle} className="text-2xl cursor-pointer rounded-md" > {theme === "light" ? "☀️" : "🌙"}</spna>
+           <div className="p-1">
+            <NavLink to="/content" ><p className="md:block hidden rounded-md px-2 py-1 b border-2 bg-zinc-100 text-black font-medium  cursor-pointer">Start Now</p></NavLink>
+           </div>
+            {/*Toggle section*/}
           <div className="md:hidden mr-2 text-white">
           <button  onClick={toogle}>
-            {isshow ? (<Menu />
-            ):(
+            {isshow ? (
               <CircleChevronRight />
+            ):(
+              <Menu />
             )}
           </button>
             </div>
+            </div>
           {isshow && (
-            <div className={`absolute w-1/2 mt-2 bg-white border transition-all duration-300 md:hidden rounded-md  top-full right-0  flex-col ${isshow ?' flex flex-col':"hidden"}`}>
+            <div className={`absolute w-1/2 mt-2 border transition-all duration-300 md:hidden rounded-md  top-full right-0  flex-col ${isshow ?' flex flex-col bg-white':"hidden"}`}>
                {links.map((items)=>(
-                 <NavLink key={items.id} to={items.link} className="px-5 py-2 hover:bg-gray-100 hover:text-black" >{items.name}</NavLink>
+                 <NavLink key={items.id} to={items.link} className="px-5 py-2 font-semibold font-mono hover:bg-gray-100 hover:text-black" >{items.name}</NavLink>
                 ))}
             </div>
           )}
