71d0455b347a3e81bfe5cee1942a5fa61a4a33a0
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-01-24 15:56:34 +0530

    Complete the Singup page With Auth

--- to-do/src/Route/Signup.jsx
+++ to-do/src/Route/Signup.jsx
@@ -3,71 +3,121 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useFirebase } from "../firebase/Firebase";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+
 const Signup = () => {
-    const { singup ,userName ,setUserName } = useFirebase();
+    const { singup } = useFirebase();
     const navigate = useNavigate();
-    
-    const handleSignup = async (e) => {
-       e.preventDefault();
-       try {
-          await singup(email, password);
-          localStorage.setItem("username", user.email)
-          console.log(email)
-          setTimeout(() => {
-            navigate("/content");
-          }, 1000);
-          setEmail("");
-          setPassword('');
-         } catch (err) {
-            console.log(err)
-            setError(err.message);
-         }
-      };
-       const showpassword=(e)=>{
-         e.preventDefault()
-         setShowpassword((prevpassword)=>!prevpassword)
-       }
+    const [userName, setUserName] = useState("")
     const [error, setError] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword ,setShowpassword]=useState(false)
 
+    const handleSignup = async (e) => {
+       e.preventDefault();
+       // password vaildation checking
+          if(password.length >8){
+            setError('Password must be at least 8 characters')
+            return false
+          }
+          if (!/[A-Z]/.test(password)) {
+            setError('Need 1 uppercase letter');
+            return false;
+          }
+          if (!/[a-z]/.test(password)) {
+            setError('Need 1 lowercase letter');
+            return false;
+          }
+          if (!/[0-9]/.test(password)) {
+            setError('Need 1 number');
+            return false;
+          }
+          setError('');
+          // If all Valiad 
+          try {
+            const user=await singup(email, password,userName);
+            localStorage.setItem("UserEmail",user.email)
+            localStorage.setItem("DisplayName",userName)
+          setTimeout(() => {
+             navigate("/content");
+            }, 1000);
+            setUserName("")
+            setEmail("");
+            setPassword('');
+         } catch (err) {
+            console.log(err)
+            setError(err.message);
+         }
+      };
+      const showpassword=(e)=>{
+         e.preventDefault()
+         setShowpassword((prevpassword)=>!prevpassword)
+      }
+    
+
     return (
- <section className="min-h-screen  flex flex-col  justify-center items-center ">
-    <div className='p-5  bg-white rounded-md shadow-2xl w-80 '>
-      <h2 className='flex justify-center text-xl font-jetbrains font-semibold'>Create Account </h2>
-
-       <div className='space-y-2 '> 
-       {/*User Name*/}
-       <div className="relative w-full justify-center items-center ">
-           <label className='font-jetbrains  font-semibold text-sm'>UserName</label>
-           <input type="email" className="px-4 py-3 ml-2  placeholder:font-mono tracking-tighter font-semibold rounded-lg outline-none transition-transform duration-150" placeholder="Enter your email" value={userName} onChange={(e) => setUserName(e.target.value)} />
-           </div>
-
-           {/*Email Section*/}
-          <div className="relative w-full justify-center items-center ">
-           <label className='font-jetbrains  font-semibold text-sm'>Email</label>
-           <input type="email" className="px-4 py-3 ml-2  placeholder:font-mono tracking-tighter font-semibold rounded-lg outline-none transition-transform duration-150" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
-           </div>
-           {/*Password  */}
-
-            <div className="relative  flex flex-row  justify-center items-center">
-               <label className=' font-jetbrains font-semibold text-sm'>Password</label>
-            <input type={showPassword ? "text" : "password"} className="px-4 py-3 placeholder:font-mono tracking-tighter font-semibold rounded-lg outline-none transition-transform duration-150  w-full pr-10" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} />
-             <button type="button" onClick={showpassword} className="absolute right-3 bottom-0 top-1 bg-transparent border-none cursor-pointer text-black">
+      <section className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
+      <div className="px-6 py-8 bg-white rounded-md shadow-lg w-[80%] sm:w-[500px]">
+        <h2 className="text-2xl font-semibold text-center mb-5 font-mono">Create Account</h2>
+    
+        <div className="space-y-4">
+          {/* Username */}
+          <div className="flex flex-row justify-between items-center">
+            <label className="font-semibold text-sm w-[30%]">UserName</label>
+            <input
+              type="text"
+              className="px-4 py-3 w-[65%] placeholder:font-mono font-semibold rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-row justify-between items-center">
+            <label className="font-semibold text-sm w-[30%]">Email</label>
+            <input
+              type="email"
+              className="px-4 py-3 w-[65%] placeholder:font-mono font-semibold rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-row justify-between items-center relative">
+            <label className="font-semibold text-sm w-[30%]">Password</label>
+            <input
+              type={showPassword ? "text" : "password"}
+              className="px-4 py-3 w-[65%] placeholder:font-mono font-semibold rounded-lg border border-gray-300 outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+            <button type="button"  onClick={showpassword}
+              className="absolute right-4 top-4 bg-transparent border-none cursor-pointer text-black"  >
               {showPassword ? <FaEye /> : <FaEyeSlash />}
-             </button>
-             </div>
-             <p className="text-gray-600 font-mono">{error}</p>
-            </div>
-             <div className='flex justify-center mt-4'>
-                <button onClick={handleSignup} type="submit" className=' bg-black px-2 py-1 w-full rounded-lg text-white font-special text-xl font-mono hover:bg-gray-900'>Signup</button>
-             </div>
-              <div className="mt-2 font-mono tracking-tighter flex flex-row justify-center items-center">
-               <p>Have already account?<NavLink to="/login" ><span className="underline underline-offset-2 font-mono">Login</span></NavLink></p>
-              </div>
-           </div>
-        </section>
+            </button>
+          </div>
+    
+          <p className="text-red-500 text-sm text-center">{error}</p>
+        </div>
+    
+        <div className="flex justify-center mt-6">
+          <button
+            onClick={handleSignup}
+            type="submit"
+            className="bg-gray-950 px-4 py-2 w-full rounded-lg text-white font-semibold text-xl hover:bg-gray-900"  >
+            Signup
+          </button>
+        </div>
+   
+        <div className="mt-4 text-center">
+          <p className="text-sm font-semibold">
+            Already have an account?{' '}
+            <NavLink to="/login" className="font-semibold text-blue-500 hover:underline"> Login  </NavLink>
+          </p>
+        </div>
+      </div>
+    </section>
+    
+    
     );
 }
 export default Signup;
