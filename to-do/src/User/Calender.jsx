c3ed5ad89e7b5c7c238db70f317146dff5037e5b
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-01-26 23:19:55 +0530

    Siderbar Done

---
+++ to-do/src/User/Calender.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ThemeContext } from '../theme/ThemeContext';
+import { useContext } from 'react';
+const Calender=() =>{
+  const {theme} =useContext(ThemeContext)
+    return (
+      <section className={` bg-slate-500  h-screen  ${theme==="dark" ? "bg-zinc-800 text-white":"bg-white text-black"}`}>
+      <p>Hello Home</p>
+    </section>
+  )
+}
+export default Calender;
