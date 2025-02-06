0018a9ba2b7b969d282eda2e45d613e2c287d06b
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-02-06 23:40:40 +0530

    Need make some Proggess Things

--- to-do/src/pages/subpage/sidebar/Sidebar.jsx
+++ to-do/src/pages/subpage/sidebar/Sidebar.jsx
@@ -43,11 +43,11 @@ export default function Sidebar({className}) {
       setTheme((prevtheme)=>(prevtheme==="light"?"dark":"light"))
      }
   return (
-    <div className={` fixed top-0 left-0 h-full transition-all  ease-in-out    duration-75 ${ sidebarShow ? "w-" : "w-16" } flex flex-col ${className} 
+    <div className={` fixed top-0 left-0 h-full transition-all  ease-in-out    duration-75 ${ sidebarShow ? "w-" : "w-12" } flex flex-col ${className} 
     ${theme === "dark" ? "bg-zinc-900  text-white" : "bg-white text-black"}   `}  >
     <button
       onClick={toggle}
-      className={`absolute text-xl p-2   0  px-2  rounded-lg sm:${sidebarShow} `}  >
+      className={`absolute text-xl p-2     px-2  rounded-lg sm:${sidebarShow} `}  >
       {sidebarShow ?  (<IoIosArrowBack  size={25} strokeWidth={1.5} />) : (<FiArrowRight   size={25} strokeWidth={1.5}/>) }
     </button>
     <nav className="space-y-2 py-2 h-full flex-col gap-10 px-2 flex">
@@ -55,12 +55,12 @@ export default function Sidebar({className}) {
         <NavLink
           key={link.id}
           to={link.link}
-          className={`flex mt-20 items-center    px-2 py-1  rounded-md  ${theme ==="dark" ?" bg-zinc-800 hover:bg-zinc-600":"bg-slate-200 hover:bg-slate-400"}`} >
-          <span className='text-xl'>{link.icons}</span>
-          {sidebarShow && <span className={`ml-2 font-sans font-semibold `}>{link.name}</span>}
+          className={`flex mt-20 items-center  ${ sidebarShow ? "w-" : "w-8" } px-1 py-1  rounded-md  ${theme ==="dark" ?" bg-zinc-800 hover:bg-zinc-600":"bg-slate-200 hover:bg-slate-400"}`} >
+          <span className='text-sm'>{link.icons}</span>
+          {sidebarShow && <span className={` font-sans font-semibold `}>{link.name}</span>}
         </NavLink>
       ))}
-      <div className= {`space-y-2  absolute bottom-6  flex flex-row items-center justify-center  rounded-lg    ${sidebarShow ? "w-20 ml-7 ":"w-10 ml-0 " }  ${theme ==="dark" ?"bg-gray-800 hover:bg-zinc-700":"bg-slate-200 hover:bg-slate-400"}` }>
+      <div className= {`space-y-2  absolute bottom-6  flex flex-row items-center justify-center  rounded-lg    ${sidebarShow ? "w-20 ml-3 ":"w-8 ml-0 " }  ${theme ==="dark" ?"bg-gray-800 hover:bg-zinc-700":"bg-slate-200 hover:bg-slate-400"}` }>
          <button onClick={ThemeToggle} className='flex items-center justify-center px-4 py-2 rounded-md'>
          {theme === "light" ? <IoSunny size={20}  /> : <LuSunMoon size={20} />}
           </button>
