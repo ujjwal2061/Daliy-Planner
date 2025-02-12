959b4b2c879774ccd76771e91ca9db6f68e87d91
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-02-12 23:22:31 +0530

    Scroll aniamtion

--- to-do/src/pages/Mainpage.jsx
+++ to-do/src/pages/Mainpage.jsx
@@ -3,13 +3,24 @@ import  Home  from "./subpage/Home"
 import  Footer  from "./subpage/Footer"
 import  Contact  from "./subpage/Contact"
 import Feature from "./subpage/Feature"
+import { useRef } from 'react'
 const Manipage=()=>{
+  const homeRef = useRef(null);
+  const featureRef = useRef(null);
+  const contactRef = useRef(null);
+  const footerRef = useRef(null);
+    const scrollToSection = (ref) => {
+      if (ref.current) {
+        ref.current.scrollIntoView({ behavior: "smooth", block: "start" });
+      }
+    };
+  
     return(
       <Screen>
-        <Home />
-        <Feature />
-        <Contact />
-        <Footer />
+          <Home  homeref={homeRef} scrollToSection={scrollToSection} featureRef={featureRef} contactRef={contactRef} />
+      <Feature ref={featureRef} />
+      <Contact ref={contactRef} />
+      <Footer ref={footerRef} />
       </Screen>
     )
 }
