db7c71cafdf3871df504ee79baa75df2ff2c9861
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-02-16 03:37:31 +0530

    done

--- to-do/src/pages/subpage/Contact.jsx
+++ to-do/src/pages/subpage/Contact.jsx
@@ -1,24 +1,24 @@
 import { ThemeContext } from "../../theme/ThemeContext";
 import React, { useContext, useState } from "react";
-import { IoMdAdd } from "react-icons/io";
+import { FaChevronUp } from "react-icons/fa";
+import { FaChevronDown } from "react-icons/fa";
 const Contact = React.forwardRef((props, ref) => {
   const { theme } = useContext(ThemeContext);
-  const [show ,setShow]=useState(false)
+  const [expandIndex,setexpandIndex]=useState(null)
 const listshow=[
-  { head:"AI-Generated Timetable" ,text:" Automatically creates a daily schedule based on your tasks and priorities."},
-  {   head:"Calendar Integration" ,text:" Sync your tasks with a built-in calendar to plan for upcoming deadlines and events."},
+  { head:"AI Gen-Timetable" ,text:" Automatically creates a daily schedule based on your tasks and priorities."},
+  {  head:"Calendar Integration" ,text:" Sync your tasks with a built-in calendar to plan for upcoming deadlines and events."},
   {  head:"Study with Friends" ,text:" Invite friends to a shared timer session and stay motivated together."},
   {  head: "Progress Tracking" ,text:" Monitor your productivity and see your improvements over time."},
 ]
-const isshow=()=>{
-  setShow((preveshow)=>!preveshow)
+const hanvdleExpand=(index)=>{
+  setexpandIndex(expandIndex === index ?null :index);
 }
   return (
     <section 
       ref={ref} 
-      className={`px-4 py-16 ${theme === 'dark' ? "bg-black text-white" : "bg-white text-black"}`} >
+      className={`px-4 py-11 ${theme === 'dark' ? "bg-black text-white" : "bg-white text-black"}`} >
       <div className={`max-w-5xl mx-auto text-center md:text-left ${theme === 'dark' ? "text-white":"text-black"} `}>
- 
         <h1 className="  px-3 md:px-6 font-myfont underline  selection:bg-highlight selection:text-black 
           font-semibold text-2xl md:text-3xl lg:text-4xl">
           Why Use the Daily Planner?
@@ -27,16 +27,19 @@ const isshow=()=>{
           Our AI-powered Daily Planner helps you stay organized, maximize productivity, and collaborate with friends.  
           Here’s what makes it unique
         </p>
-        <ul className="mt-6 space-y-3 px-4">
-      
+ <ul className="mt-6   space-y-3 px-4">
   {listshow.map((items, index) => (
-    <li
-      key={index}
-      className={`px-4 py-4 flex flex-wrap sm:flex-nowrap items-center justify-between rounded-lg 
+    <li key={index} className={`px-4 py-4 flex flex-wrap  flex-col  gap-2 sm:flex-nowrap items-center justify-between rounded-lg 
         ${theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-black shadow-lg"}`} >
-      <strong className="w-full sm:w-1/4 text-left font-bold">{items.head}</strong>
-      <p className="flex-1 text-left break-words">{items.text}</p>
-      <button> <IoMdAdd className="text-2xl ml-3" /></button>
+       <div className=" w-full flex justify-between">
+         <strong className="w-full sm:w-1/4 text-left font-bold">{items.head}</strong><br />
+         <button onClick={()=>hanvdleExpand(index)}> {expandIndex ===index ?<FaChevronUp className="text-xl ml-3" />:<FaChevronDown className="text-xl ml-3"/>}</button>
+       </div>
+       <div className={`mr-auto transition-all  duration-1000 ease-in-out ${expandIndex === index ? "max-h-96 opacity-100" : "max-h-0   opacity-0 overflow-hidden"}`}>
+            {expandIndex === index && (
+              <p className="mt-2  font-lightfont text-left break-words">{items.text}</p>
+            )}
+          </div>
     </li>
   ))}
 </ul>
