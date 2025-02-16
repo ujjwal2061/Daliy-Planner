370830e1c8718f1520aa8f31674ec82055a66835
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-02-16 14:37:58 +0530

    Complete

--- to-do/src/pages/subpage/Contact.jsx
+++ to-do/src/pages/subpage/Contact.jsx
@@ -17,8 +17,8 @@ const hanvdleExpand=(index)=>{
   return (
     <section 
       ref={ref} 
-      className={`px-4 py-11 ${theme === 'dark' ? "bg-black text-white" : "bg-white text-black"}`} >
-      <div className={`max-w-5xl mx-auto text-center md:text-left ${theme === 'dark' ? "text-white":"text-black"} `}>
+      className={` relative z-0 px-4 py-11 ${theme === 'dark' ? "bg-black text-white" : "bg-white text-black"}`} >
+ <div className={`px-3 py-5 rounded-md max-w-5xl mx-auto text-center md:text-left ${theme === 'dark' ? "bg-gradient-to-r from-black via-gray-900  to-black shadow-xl text-white" : " shadow-2xl text-black"}`}>
         <h1 className="  px-3 md:px-6 font-myfont underline  selection:bg-highlight selection:text-black 
           font-semibold text-2xl md:text-3xl lg:text-4xl">
           Why Use the Daily Planner?
@@ -27,10 +27,18 @@ const hanvdleExpand=(index)=>{
           Our AI-powered Daily Planner helps you stay organized, maximize productivity, and collaborate with friends.  
           Here’s what makes it unique
         </p>
- <ul className="mt-6   space-y-3 px-4">
+ <ul className="mt-6 z-10 space-y-3 px-4">
   {listshow.map((items, index) => (
-    <li key={index} className={`px-4 py-4 flex flex-wrap  flex-col  gap-2 sm:flex-nowrap items-center justify-between rounded-lg 
-        ${theme === "dark" ? "bg-gray-900 text-white" : "bg-white text-black shadow-lg"}`} >
+  <li key={index} className={`z-20 px-4 py-4 flex flex-wrap flex-col gap-2 sm:flex-nowrap items-center justify-between rounded-lg transition-all duration-500 ease-in-out 
+    ${theme === "dark" 
+      ? expandIndex === index 
+        ? "bg-gray-900 opacity-100 border-2 text-white" 
+        : "bg-transparent opacity-80 border-2 text-white" 
+      : expandIndex === index 
+        ? "bg-white text-black shadow-lg opacity-100" 
+        : "bg-white text-black shadow-lg opacity-50"
+    }`}>
+  
        <div className=" w-full flex justify-between">
          <strong className="w-full sm:w-1/4 text-left font-bold">{items.head}</strong><br />
          <button onClick={()=>hanvdleExpand(index)}> {expandIndex ===index ?<FaChevronUp className="text-xl ml-3" />:<FaChevronDown className="text-xl ml-3"/>}</button>
